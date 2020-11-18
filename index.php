e2378d3693d3063aca4ac1192783347be02bcd35
StanislasE <stanislas.etienne@gmail.com>
2020-11-18 15:13:38 +0100

    fait

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 4</title>
+</head>
+<body>
+    
+<?php
+    $name = 'Etienne';
+    echo 'Bonjour  ' . $name  . ' comment vas-tu ?' // Le point sert à concaténer les valeurs
+?>
+
+</body>
+</html>
